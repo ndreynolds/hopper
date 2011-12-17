ddeed00d6cf2896f6f57423125c2a03b13bc61e5
Nick Reynolds <ndreynolds@gmail.com>
2011-12-16 21:40:46 -0500

    Working settings page.

--- hopper/web/views/settings.py
+++ hopper/web/views/settings.py
@@ -1,12 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from hopper.web.utils import setup
 
 settings = Blueprint('settings', __name__)
 
-@settings.route('/')
+@settings.route('/', methods=['GET', 'POST'])
 def main():
     tracker, config = setup()
+    if request.method == 'POST':
+        tracker.config.name = request.form['name']
+        tracker.config.save()
     header = 'Settings for %s' % tracker.config.name
     return render_template('settings.html', tracker=tracker, 
-                           selected='settings', header=header)
+                           name=tracker.config.name, selected='settings', 
+                           header=header)
